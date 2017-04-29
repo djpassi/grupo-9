f5da52dfe9045955e1e2988b2c2077d08e0ce7fe
djpassi <djpassi@uc.cl>
2017-04-29 19:33:02 -0300

    Created Project form and validations

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html do
+          redirect_to '/projects/new', notice: 'Project was successfully created.'
+        end
+      else
+        format.html { render :new, status: 422 }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :user_id, :goal,
+     :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/projects/new', to: 'projects#new'
+  post '/projects', to: 'projects#create'
 end
