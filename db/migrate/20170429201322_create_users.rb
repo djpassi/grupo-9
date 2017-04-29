69fe6dd705932e1da9672cfc75d0fa3091befffb
djpassi <djpassi@uc.cl>
2017-04-29 17:18:53 -0300

    Created some models
    user and Category

---
+++ db/migrate/20170429201322_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :sex
+      t.date :birth_date
+      t.string :password
+      t.string :description
+      t.string :image
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
