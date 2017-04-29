e02b5c1a7f81b73a1733a18f8ed324a6be05566e
David Galemiri <dagalemiri@uc.cl>
2017-04-29 17:28:45 -0300

    Created NN relation
    Between User and category

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  has_many :likes
+  has_many :categories, through: :likes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429201350) do
+ActiveRecord::Schema.define(version: 20170429202135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170429201350) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_likes_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 20170429201350) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "likes", "categories"
+  add_foreign_key "likes", "users"
 end
