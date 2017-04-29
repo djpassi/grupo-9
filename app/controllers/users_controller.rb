a85b8c9b9ef0e7e5562f1c908eb8db11daa2f214
David Galemiri <dagalemiri@uc.cl>
2017-04-29 19:32:41 -0300

    Completed user from

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,35 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.role = "User"
 
-    if @user.save
-        redirect_to '/users/new', notice: "User created"
-    else
-        redirect_to '/users/new', notice: "User could not be created"
-    end
-  end
+    respond_to do |format|
+     if @user.save
+       format.html do
+         redirect_to '/users/new', notice: 'User was successfully created.'
+       end
+       #format.json { render :show, status: :created, location: @user }
+     else
+       format.html { render :new, status: 422 }
+       format.json { render json: @user.errors, status: :unprocessable_entity }
+     end
+   end
+
+ end
+
+
+
+
+  #   if @user.save
+  #       redirect_to '/users/new', notice: "User created"
+  #   else
+  #       redirect_to '/users/new', notice: "User could not be created"
+  #   end
+  # end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email,
-     :password, :birth_date)
+     :password, :birth_date, :password_confirmation)
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 
-  validates :first_name, presence: true
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
+  validates :password, confirmation:true, presence: true, length: { minimum: 6 }
+  validates :email, uniqueness:true, presence: true, allow_blank: false, format: {with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}
+
 
   has_many :likes
   has_many :categories, through: :likes
