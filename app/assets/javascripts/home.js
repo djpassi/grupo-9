f7847d59c8d0ed14d96ca4e04febd7b4ff41defe
djpassi <djpassi@uc.cl>
2017-06-11 19:32:31 -0400

    Normalized navbar and sidebar with style

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(document).ready(function() {
-  $("[data-toggle]").click(function() {
-    var toggle_el = $(this).data("toggle");
-    $(toggle_el).toggleClass("open-sidebar");
-    $('#sidebar').toggleClass('change-sidebar');
-  });
-
-});
