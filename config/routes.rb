11433a59ebb8e17ff9a49e71ff8cffc8042ec0c5
David Galemiri <dagalemiri@uc.cl>
2017-06-26 18:15:22 -0300

    Added comments and investments API

--- config/routes.rb
+++ config/routes.rb
@@ -53,10 +53,11 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
 
-
   namespace :api do
    namespace :v1 do
      resources :projects, only: [:index, :show]
+     resources :comments, only: [:show, :create,:destroy]
+     resources :investments, only: [:create]
    end
  end
 
